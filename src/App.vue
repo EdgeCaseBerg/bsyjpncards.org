fddaff539760e65bdd73ea7d0365a3900135239a
Ethan Eldridge <ejayeldridge@gmail.com>
2022-02-21 18:53:00 -0500

    Post HW 1 and caught up with 1st 1/2 of book 2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Study</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/何">何</router-link>
   </div>
   <router-view/>
 </template>
---
+++ src/views/何.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="centerme">
+        <p class="vertical" lang="ja">
+            何ですかね<br/>
+            カードを勉強しました<br/>
+            そして、もっと 勉強します
+        </p>
+    </div>
+</template>
+<style scoped>
+html, .vertical {
+    -webkit-writing-mode: vertical-rl;
+    -moz-writing-mode: vertical-rl;
+    -ms-writing-mode: vertical-rl;
+    writing-mode: vertical-rl;
+    margin: 2em;
+    vertical-align: top;
+    text-align: start;
+}
+
+.centerme {
+   padding-left: 46vw;
+}
+</style>
