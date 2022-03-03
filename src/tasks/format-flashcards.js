56aff4ad92a8c4cc39387cf99383af67223e4df0
Ethan Eldridge <ejayeldridge@gmail.com>
2022-03-03 17:31:34 -0500

    Make flashcard formatter overwrite flashcard file

--- src/tasks/format-flashcards.js
+++ src/tasks/format-flashcards.js
@@ -3,4 +3,6 @@ const flashcards = require('../assets/flashcards.json')
 
 const formattedText = JSON.stringify(flashcards, null, '    ')
 
-fs.writeFile('./../assets/flashcards.json', formattedText, console.error)
+fs.writeFile('src/assets/flashcards.json', formattedText, console.error)
+
+console.log('Formatted flashcards')
