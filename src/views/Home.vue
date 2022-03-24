2cb78d4bd08ecc4a7b890324e098cad6c2b8736b
Ethan Eldridge <ejayeldridge@gmail.com>
2022-03-24 01:34:14 -0400

    Save what flashcards have been seen for up to 30 days
    This was fun. To save what we see we convert the data into a bitstring
    where a 0 in the 2 ^ position indicates that we saw the card at position
    in the originals list. So 0 means we saw nothing, while a 1 would
    indicate we saw only the first card in the list. The beauty of it of
    course is that '010' means you saw the 2nd card, and '011' means you saw
    the second and first card but it can be stored as the number 3. Which is
    just the sum of the two 2 ^ position values. So it's pretty easy to
    encode.
    
    since we have over 900 cards, we need to represent 2 ^ 900 of course,
    which is bigger than Javascript can handle on its own, hence the
    BigInteger class we're using.
    
    The BitInteger number is stored in a cookie for up to 30 days and is
    decoded on load and saved each time we get a new card.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -41,6 +41,7 @@
 // @ is an alias to /src
 import FlashCard from '@/components/FlashCard.vue'
 import cards from '@/assets/flashcards.json'
+const BigInteger = require('jsbn').BigInteger
 
 const originals = []
 for (let i = 0; i < cards.length; i++) {
@@ -54,14 +55,13 @@ export default {
   },
   data () {
     return {
-      seenToday: [],
+      seen: [],
       flashcards: cards,
       card: {}
     }
   },
   methods: {
     getCard: function () {
-      console.log(this.flashcards)
       if (this.flashcards.length <= 0) {
         this.card = {
           fronttext: 'カードもなにもありません',
@@ -74,14 +74,40 @@ export default {
       const idx = Math.floor(Math.random(100) * this.flashcards.length)
       const card = this.flashcards[idx]
       this.flashcards.splice(idx, 1)
-      this.seenToday.push(card)
       this.card = card
+
+      for (let index = 0; index < originals.length; index++) {
+        const element = originals[index]
+        if (element.fronttext === card.fronttext && element.backtext === card.backtext) {
+          this.seen.push(index)
+        }
+      }
+      document.cookie = `seen=${this.encodeSeen()}; max-age=${60 * 60 * 24 * 30}`
+    },
+    encodeSeen () {
+      let bitString = new BigInteger('0')
+      for (let index = 0; index < this.seen.length; index++) {
+        const indexSeen = this.seen[index]
+        const bitToEnable = new BigInteger('2').pow(indexSeen)
+        bitString += bitToEnable
+      }
+      return bitString
+    },
+    decodeCodeSeen: function (bigInteger) {
+      const bitString = bigInteger.toString(2)
+      const decodedSeen = []
+      for (let index = bitString.length - 1; index >= 0; index--) {
+        const element = bitString[index]
+        if (element === '1') {
+          decodedSeen.push(bitString.length - 1 - index)
+        }
+      }
+      return decodedSeen
     },
     filterTo: function (source, upToPage) {
       const newArray = []
       for (let index = 0; index < originals.length; index++) {
         const element = originals[index]
-        console.log(upToPage)
         if (
           (element.source === source && upToPage === null) ||
           (element.source === source && element.page && upToPage && element.page < upToPage)
@@ -99,6 +125,7 @@ export default {
         newArray.push(element)
       }
       this.flashcards = newArray
+      this.seen = []
       this.getCard()
     },
     filterKanji: function () {
@@ -114,6 +141,15 @@ export default {
     }
   },
   created () {
+    this.cards = originals
+    if (this.cookie) {
+      const encodedSeen = new BigInteger(this.cookie)
+      this.seen = this.decodeCodeSeen(encodedSeen)
+      /* We now have indices we've seen before, so make a version of the originals that has these removed */
+      this.flashcards = this.cards.filter((value, index) => {
+        return this.seen.indexOf(index) === -1
+      })
+    }
     this.getCard()
   },
   mounted () {
@@ -125,6 +161,17 @@ export default {
         return 0
       }
       return this.flashcards.length
+    },
+    cookie: function () {
+      const cookieArray = document.cookie.split(';')
+      for (let index = 0; index < cookieArray.length; index++) {
+        const element = cookieArray[index]
+        const cookieName = element.split('=')[0]
+        if (cookieName === 'seen') {
+          return element.split('=')[1]
+        }
+      }
+      return '0'
     }
   }
 }
